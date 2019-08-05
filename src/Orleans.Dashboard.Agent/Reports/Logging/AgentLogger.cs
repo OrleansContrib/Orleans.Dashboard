6fad31001bfbfdfebd8705b448c28e088ea50b68
Gutemberg Ribeiro <gutemberg@outlook.com>
2019-08-05 19:52:04 -0300

    Enhanced log message

--- src/Orleans.Dashboard.Agent/Reports/Logging/AgentLogger.cs
+++ src/Orleans.Dashboard.Agent/Reports/Logging/AgentLogger.cs
@@ -1,11 +1,16 @@
 using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Globalization;
+using System.Text;
 using Microsoft.Extensions.Logging;
-using Orleans.Runtime;
 
 namespace Orleans.Dashboard.Reports.Logging
 {
     internal class AgentLogger : ILogger
     {
+        private const string ScopeKey = "Scope";
+        private const string ArrowToken = " => ";
         private readonly string _categoryName;
         private readonly AgentLoggerOptions _options;
         private readonly IAgentMessageWriter _messageWriter;
@@ -36,12 +41,57 @@ namespace Orleans.Dashboard.Reports.Logging
             {
                 var message = new LogMessage
                 {
+                    Category = this._categoryName,
                     Level = logLevel,
-                    EventId = eventId,
-                    Content = formatter(state, exception)
+                    EventId = eventId.Id != 0 ? eventId.Id.ToString(CultureInfo.InvariantCulture) : string.Empty,
+                    EventName = !string.IsNullOrWhiteSpace(eventId.Name) ? eventId.Name : string.Empty,
+                    Content = formatter(state, exception),
+                    ActivityId = Activity.Current?.Id,
+                    DateTime = DateTime.UtcNow
                 };
 
-                var a = RequestContext.PropagateActivityId;
+                if (this._options.IncludeScopes)
+                {
+                    message.Properties = new Dictionary<string, string>();
+
+                    if (state is IReadOnlyCollection<KeyValuePair<string, object>> stateDictionary)
+                    {
+                        foreach (KeyValuePair<string, object> item in stateDictionary)
+                        {
+                            message.Properties[item.Key] = Convert.ToString(item.Value, CultureInfo.InvariantCulture);
+                        }
+                    }
+
+                    if (this.ExternalScopeProvider != null)
+                    {
+                        var sb = new StringBuilder();
+                        this.ExternalScopeProvider.ForEachScope(
+                            (activeScope, builder) =>
+                            {
+                                // Ideally we expect that the scope to implement IReadOnlyList<KeyValuePair<string, object>>.
+                                // But this is not guaranteed as user can call BeginScope and pass anything. Hence
+                                // we try to resolve the scope as Dictionary and if we fail, we just serialize the object and add it.
+
+                                if (activeScope is IReadOnlyCollection<KeyValuePair<string, object>> activeScopeDictionary)
+                                {
+                                    foreach (KeyValuePair<string, object> item in activeScopeDictionary)
+                                    {
+                                        message.Properties[item.Key] = Convert.ToString(item.Value, CultureInfo.InvariantCulture);
+                                    }
+                                }
+                                else
+                                {
+                                    builder.Append(ArrowToken).Append(activeScope);
+                                }
+                            },
+                            sb);
+
+                        if (sb.Length > 0)
+                        {
+                            message.Properties[ScopeKey] = sb.ToString();
+                        }
+                    }
+                }
 
                 this._messageWriter.Write(new ReportMessage { Type = ReportMessageType.Log, Payload = message });
             }
--- src/Orleans.Dashboard.Agent/Reports/Logging/LogMessage.cs
+++ src/Orleans.Dashboard.Agent/Reports/Logging/LogMessage.cs
@@ -1,11 +1,18 @@
+using System;
+using System.Collections.Generic;
 using Microsoft.Extensions.Logging;
 
 namespace Orleans.Dashboard.Reports.Logging
 {
     internal class LogMessage
     {
+        public DateTime DateTime { get; set; }
         public LogLevel Level { get; set; }
-        public EventId EventId { get; set; }
+        public string EventId { get; set; }
+        public string EventName { get; set; }
+        public string Category { get; set; }
         public string Content { get; set; }
+        public string ActivityId { get; set; }
+        public IDictionary<string, string> Properties { get; set; }
     }
 }
